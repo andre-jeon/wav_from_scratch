d2452a235d570fff59f8ad0293637b7c0809e0d9
Myeongwon André Jeon <maj@Myeongwons-MBP.fios-router.home>
2021-06-21 19:22:39 -0400

    looking at the documentation

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 #include <fstream>
 using namespace std;
 
+// 44.1khz
 const int sampleRate = 44100;
+const int bitDepth = 16;
 
 class SineOscillator {
     float frequency, amplitude, angle = 0.0f, offset = 0.0;
@@ -21,7 +23,9 @@ public:
     }
 };
 
-
+void writeToFile(ofstream &file, int value, int size) {
+    file.write(reinterpret_cast<const char*> (&value), size);
+}
 
 int main() {
     int duration = 2;
@@ -30,8 +34,20 @@ int main() {
 
     SineOscillator sineOscillator(440, 0.5);
 
+    // Header Chunk
+    audioFile << "RIFF";
+    audioFile << "----";
+    audioFile << "WAVE";
+
+    // Audio Chunk
+    audioFile << "fmt ";
+    writeToFile(audioFile, 16, 4);
+
+    auto float maxAmplitude = pow(2, bitDepth - 1) -1;
     for(int i = 0; i < sampleRate * duration; i++) {
-        audioFile << sineOscillator.process() << endl;
+        auto float sample = sineOscillator.process();
+        int intSample = static_cast<int> (sample * maxAmplitude);
+        writeToFile(audioFile, intSample, 2);
     }
 
     audioFile.close();
