43cb273a4909f3ea5114c0d4035663777309eb41
Myeongwon André Jeon <maj@Myeongwons-MBP.fios-router.home>
2021-06-21 18:58:19 -0400

    okay?

--- main.cpp
+++ main.cpp
@@ -1,14 +1,39 @@
 #include <iostream>
+#include <cmath>
+#include <fstream>
+using namespace std;
+
+const int sampleRate = 44100;
 
 class SineOscillator {
-    float frequency, amplitude;
+    float frequency, amplitude, angle = 0.0f, offset = 0.0;
+
 public:
-    SineOscillator(float freq, float amp) : frequency(freq), amplitude(amp);
+    SineOscillator(float freq, float amp) : frequency(freq), amplitude(amp) {
+        offset = 2 * M_PI * frequency / sampleRate;
+    }
+
+    float process() {
+        auto float sample =  amplitude * sin(angle);
+        // Asin(2pif/sampling rate)
+        angle += offset;
+        return sample;
+    }
 };
 
 
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int duration = 2;
+    ofstream audioFile;
+    audioFile.open("waveform", ios::binary);
+
+    SineOscillator sineOscillator(440, 0.5);
+
+    for(int i = 0; i < sampleRate * duration; i++) {
+        audioFile << sineOscillator.process() << endl;
+    }
+
+    audioFile.close();
     return 0;
 }
